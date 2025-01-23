ea3b6f4575d222067b429238927bf93ab767661c
Webster Santos <133889369+firstwebster@users.noreply.github.com>
2025-01-22 23:41:50 -0300

    APP GERAL PASTA 1

---
+++ APPMOBMEDIA/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using APPMOBMEDIA.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace APPMOBMEDIA.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ APPMOBMEDIA/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using APPMOBMEDIA.Models;
+using APPMOBMEDIA.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APPMOBMEDIA.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
